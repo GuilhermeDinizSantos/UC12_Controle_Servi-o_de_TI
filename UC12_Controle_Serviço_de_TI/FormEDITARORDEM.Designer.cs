d31a949d624b4f8024c66766b6c9bb320c5815ad
FabricioAntonio21 <antoniofabricio016@gmail.com>
2023-06-19 21:58:13 -0300

    Att

--- UC12_Controle_Serviço_de_TI/FormEDITARORDEM.Designer.cs
+++ UC12_Controle_Serviço_de_TI/FormEDITARORDEM.Designer.cs
@@ -28,12 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonALTERAR = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxOBSERVACOES = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxPROBLEMA = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSTATUS = new System.Windows.Forms.TextBox();
+            this.textBoxORCAMENTO = new System.Windows.Forms.TextBox();
+            this.textBoxDATAENTRADA = new System.Windows.Forms.TextBox();
+            this.textBoxDATAEMISSAO = new System.Windows.Forms.TextBox();
+            this.textBoxDATASAIDA = new System.Windows.Forms.TextBox();
+            this.textBoxENDERECO = new System.Windows.Forms.TextBox();
+            this.textBoxCLIENTE = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonALTERAR
+            // 
+            this.buttonALTERAR.Location = new System.Drawing.Point(245, 386);
+            this.buttonALTERAR.Name = "buttonALTERAR";
+            this.buttonALTERAR.Size = new System.Drawing.Size(75, 23);
+            this.buttonALTERAR.TabIndex = 57;
+            this.buttonALTERAR.Text = "Alterar";
+            this.buttonALTERAR.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(373, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(417, 367);
+            this.dataGridView1.TabIndex = 56;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 55;
+            this.label9.Text = "Observações:";
+            // 
+            // textBoxOBSERVACOES
+            // 
+            this.textBoxOBSERVACOES.Location = new System.Drawing.Point(14, 181);
+            this.textBoxOBSERVACOES.Name = "textBoxOBSERVACOES";
+            this.textBoxOBSERVACOES.Size = new System.Drawing.Size(306, 20);
+            this.textBoxOBSERVACOES.TabIndex = 54;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 13);
+            this.label8.TabIndex = 53;
+            this.label8.Text = "Problema apresentado:";
+            // 
+            // textBoxPROBLEMA
+            // 
+            this.textBoxPROBLEMA.Location = new System.Drawing.Point(14, 141);
+            this.textBoxPROBLEMA.Name = "textBoxPROBLEMA";
+            this.textBoxPROBLEMA.Size = new System.Drawing.Size(306, 20);
+            this.textBoxPROBLEMA.TabIndex = 52;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 370);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 51;
+            this.label7.Text = "Status:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 331);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 50;
+            this.label6.Text = "Orçamento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 13);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "Data de saída do equipamento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 13);
+            this.label4.TabIndex = 48;
+            this.label4.Text = "Data da entrada do equipamento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 13);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Data de emissão da ordem:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Endereço:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Cliente:";
+            // 
+            // textBoxSTATUS
+            // 
+            this.textBoxSTATUS.Location = new System.Drawing.Point(14, 386);
+            this.textBoxSTATUS.Name = "textBoxSTATUS";
+            this.textBoxSTATUS.Size = new System.Drawing.Size(111, 20);
+            this.textBoxSTATUS.TabIndex = 44;
+            // 
+            // textBoxORCAMENTO
+            // 
+            this.textBoxORCAMENTO.Location = new System.Drawing.Point(14, 347);
+            this.textBoxORCAMENTO.Name = "textBoxORCAMENTO";
+            this.textBoxORCAMENTO.Size = new System.Drawing.Size(111, 20);
+            this.textBoxORCAMENTO.TabIndex = 43;
+            // 
+            // textBoxDATAENTRADA
+            // 
+            this.textBoxDATAENTRADA.Location = new System.Drawing.Point(14, 259);
+            this.textBoxDATAENTRADA.Name = "textBoxDATAENTRADA";
+            this.textBoxDATAENTRADA.Size = new System.Drawing.Size(163, 20);
+            this.textBoxDATAENTRADA.TabIndex = 42;
+            // 
+            // textBoxDATAEMISSAO
+            // 
+            this.textBoxDATAEMISSAO.Location = new System.Drawing.Point(14, 220);
+            this.textBoxDATAEMISSAO.Name = "textBoxDATAEMISSAO";
+            this.textBoxDATAEMISSAO.Size = new System.Drawing.Size(163, 20);
+            this.textBoxDATAEMISSAO.TabIndex = 41;
+            // 
+            // textBoxDATASAIDA
+            // 
+            this.textBoxDATASAIDA.Location = new System.Drawing.Point(14, 305);
+            this.textBoxDATASAIDA.Name = "textBoxDATASAIDA";
+            this.textBoxDATASAIDA.Size = new System.Drawing.Size(163, 20);
+            this.textBoxDATASAIDA.TabIndex = 40;
+            // 
+            // textBoxENDERECO
+            // 
+            this.textBoxENDERECO.Location = new System.Drawing.Point(14, 101);
+            this.textBoxENDERECO.Name = "textBoxENDERECO";
+            this.textBoxENDERECO.Size = new System.Drawing.Size(306, 20);
+            this.textBoxENDERECO.TabIndex = 39;
+            // 
+            // textBoxCLIENTE
+            // 
+            this.textBoxCLIENTE.Location = new System.Drawing.Point(14, 61);
+            this.textBoxCLIENTE.Name = "textBoxCLIENTE";
+            this.textBoxCLIENTE.Size = new System.Drawing.Size(231, 20);
+            this.textBoxCLIENTE.TabIndex = 38;
+            // 
+            // FormEDITARORDEM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonALTERAR);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxOBSERVACOES);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxPROBLEMA);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSTATUS);
+            this.Controls.Add(this.textBoxORCAMENTO);
+            this.Controls.Add(this.textBoxDATAENTRADA);
+            this.Controls.Add(this.textBoxDATAEMISSAO);
+            this.Controls.Add(this.textBoxDATASAIDA);
+            this.Controls.Add(this.textBoxENDERECO);
+            this.Controls.Add(this.textBoxCLIENTE);
+            this.Name = "FormEDITARORDEM";
             this.Text = "FormEDITARORDEM";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonALTERAR;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxOBSERVACOES;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxPROBLEMA;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSTATUS;
+        private System.Windows.Forms.TextBox textBoxORCAMENTO;
+        private System.Windows.Forms.TextBox textBoxDATAENTRADA;
+        private System.Windows.Forms.TextBox textBoxDATAEMISSAO;
+        private System.Windows.Forms.TextBox textBoxDATASAIDA;
+        private System.Windows.Forms.TextBox textBoxENDERECO;
+        private System.Windows.Forms.TextBox textBoxCLIENTE;
     }
 }
